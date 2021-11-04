52952feb2a4584aceaf87067ce594a5a1d38d8cf
Federico Crespi <fdcrespi@gmail.com>
2021-11-04 05:48:34 -0300

    Se llego hasta compartir imagen en la web

--- App.js
+++ App.js
@@ -1,31 +1,67 @@
-import React from "react";
-import {Text , View, StyleSheet, Button} from "react-native";
-import { Video, AVPlaybackStatus } from 'expo-av';
+import React, {useState} from "react";
+import {Text , View, StyleSheet, Image, Button, Alert, TouchableOpacity, Platform} from "react-native";
+import image from './assets/icon.jpg';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
 
 const App = () => {
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Los permisos para acceder a la libreria debe ser aceptados");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    /* Para compartir en la web */
+    if (Platform.OS === 'web') {
+      const remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+  }
+
+  const openShareDialogAsync = async () => {
+    // si no esta disponible para compartir la imagen
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`La imagen esta disponible en : ${selectedImage.remoteUri}`);
+      return;
+    }
+    //para compartir lo que tenemos en el estado
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
+
   return (
     <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'https://node-09.zeno.fm/fnge482h268uv?rj-ttl=5&rj-tok=AAABfOY_WzkAQa2hCQKBbGYZyw',
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
+      <Text style={styles.title}>Selecciona una imagen</Text>
+      <TouchableOpacity  onPress={openImagePickerAsync}>
+        <Image style={styles.image} 
+          //source={{uri: 'https://picsum.photos/200/200'}}
+          source = {selectedImage ? { uri: selectedImage.localUri } : image}
         />
-      </View>
+        {/* <Button 
+          onPress={() => Alert.alert("Hello word")}
+          title="Click me" 
+          color="primary"
+        /> */}
+      </TouchableOpacity>
+      {
+        selectedImage  ? 
+        (
+          <TouchableOpacity style={styles.button} onPress={openShareDialogAsync}>
+            <Text style={styles.buttontext}>Compartir</Text>
+          </TouchableOpacity>
+        ) : ( null )
+      }
     </View>
   );
 };
@@ -37,6 +73,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
+  title: {
+    fontSize: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 90,
+    resizeMode: "contain",
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 7,
+    marginTop: 10
+  },
+  buttontext: {
+    color: 'yellow',
+    fontSize: 20,
+  }
 });
 
 export default App;
